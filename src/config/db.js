0d88115bb51c1b5a7a7d483271c81bcfb2c923d1
rikakus <ianpangestu082@gmail.com>
2022-06-26 14:46:46 +0700

    fixs bug

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const db = new Pool({
-  host: process.env.DB_HOST | "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
