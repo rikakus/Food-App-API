2d6423dc04bfe0218ec8e3bbb82e09287af7888a
rikakus <ianpangestu082@gmail.com>
2022-06-26 14:25:33 +0700

    fixs bug

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,23 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const db = new Pool({
-  host: process.env.DB_HOST | 'localhost',
+  host: process.env.DB_HOST | "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT | 5432
+  port: process.env.DB_PORT | 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+  console.log("Database connected successfully.");
 });
 
 module.exports = db;
